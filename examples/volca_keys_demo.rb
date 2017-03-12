2f31adddc7902286b3a16d69f116643f9369b26c
Julian Cheal <jcheal@redhat.com>
2017-03-12 17:53:13 +0000

    Rubocop fixes

--- examples/volca_keys_demo.rb
+++ examples/volca_keys_demo.rb
@@ -11,10 +11,10 @@ volca_keys.discover(:first, :output)
 volca_keys.eg_sustain(127)
 
 # Play single continuous note
-volca_keys.play("C4")
+volca_keys.play('C4')
 
 # Play single note with params
-volca_keys.play("C4", attack: 0.7, release: 4)
+volca_keys.play('C4', attack: 0.7, release: 4)
 
 # Play chord
-volca_keys.play_chord(["C4","E4", "G4"])
+volca_keys.play_chord([%w(C4 E4 G4)])
